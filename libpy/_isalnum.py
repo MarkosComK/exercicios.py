c2f072c61adf5a2c3ed0216f115f6049fd07c513
MarkosComK <markoscomks@gmail.com>
2024-12-08 09:21:28 +0000

    tests working

--- libpy/_isalnum.py
+++ libpy/_isalnum.py
@@ -1,31 +1,17 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    main.py                                            :+:      :+:    :+:    #
+#    _isalnum.py                                        :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/12/08 08:39:40 by marsoare          #+#    #+#              #
-#    Updated: 2024/12/08 09:15:00 by marsoare         ###   ########.fr        #
+#    Created: 2024/12/08 09:06:57 by marsoare          #+#    #+#              #
+#    Updated: 2024/12/08 09:19:47 by marsoare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-from header import *
+from _isalpha import _isalpha
+from _isdigit import _isdigit
 
-def test_ascii(function):
-	i = 0
-	while (i <= 177):
-		print("char: " + chr(i), function(chr(i)))
-		i += 1
-
-def test_digit(function):
-	i = -9
-	while (i <= 9):
-		print(f'value: {i}, {function(i)}')
-		i += 1
-
-def __main__():
-	test_digit(_isalnum)
-	test_ascii(_isalnum)
-
-__main__()
+def _isalnum(c) -> bool:
+	return (_isdigit(c) or _isalpha(c))
---
+++ libpy/header.py
@@ -0,0 +1,9 @@
+from _isalpha import _isalpha
+from _isdigit import _isdigit
+from _isalnum import _isalnum
+
+__all__ = [
+	'_isalpha',
+	'_isdigit',
+	'_isalnum',
+]
