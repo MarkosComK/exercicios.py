fe58c8c5432e24da59516b81b05e27fa3813e336
MarkosComK <markoscomks@gmail.com>
2024-12-08 09:26:44 +0000

    created isprint func

--- libpy/header.py
+++ libpy/header.py
@@ -1,9 +1,11 @@
 from _isalpha import _isalpha
 from _isdigit import _isdigit
 from _isalnum import _isalnum
+from _isprint import _isprint
 
 __all__ = [
 	'_isalpha',
 	'_isdigit',
 	'_isalnum',
+	'_isprint',
 ]
